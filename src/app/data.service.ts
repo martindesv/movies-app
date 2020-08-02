1001457dd123c3df0da1683d2cf9d971316fc5b4
Martin Apalkov <martin.apalkov@helpific.com>
2020-08-02 10:30:54 +0300

    url into variable

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,12 +8,14 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  url: string = 'https://www.omdbapi.com/?apikey=f79aeba3&';
+
   getData(string, pageNum) {
-    return this.http.get('https://www.omdbapi.com/?apikey=f79aeba3&s=' + string + '&page=' + pageNum);
+    return this.http.get(this.url + 's=' + string + '&page=' + pageNum);
   }
 
   getDetailsData(string) {
-    return this.http.get('https://www.omdbapi.com/?apikey=f79aeba3&i=' + string);
+    return this.http.get(this.url + 'i=' + string);
   }
   
 }
